64207533f3e40bad92e204414b45f8408112cc21
Neil Davis <neildavis@users.noreply.github.com>
2023-01-06 01:14:09 +0000

    Add script & update README

--- README.md
+++ README.md
@@ -1,2 +1,184 @@
-# mgpr-pi
-Shell script to automate install of Ben Geeves' Monaco GP Remake on Rapsberry Pi OS (32-bit)
+# mgpr-pi #
+
+## Play Monaco GP Remake on a Raspberry Pi ##
+
+[![Video](https://img.youtube.com/vi/X4zbBv5r75I/maxresdefault.jpg)](https://www.youtube.com/watch?v=X4zbBv5r75I)
+
+## Overview ##
+
+This repo contains a shell script to automate installation of Ben Geeves' amazing
+[Monaco GP Remake](http://forum.arcadecontrols.com/index.php?topic=134445.0)
+game on [Raspberry Pi OS](https://www.raspberrypi.com/software/).
+
+Ben only released binaries of this game for Windows & Linux systems using Intel/AMD x86/x64 CPUs.
+Since the Raspberry Pi uses an ARM CPU, the game will not run on Raspberry Pi OS - at least not natively.
+
+Since source code is not available, I decided to see if it was possible to use emulation to run the
+game on a Pi, and was delighted to discover that it is indeed possible, thanks in a large part to the magic of
+[box86](https://github.com/ptitSeb/box86).
+
+The process of getting it running was still not simple though, due to the requirement to hunt down x86 versions of
+several libraries that the game depended on, but are not yet natively 'wrapped' by box86.
+Also, the game requires X11, and whilst that means it will run fine in the Desktop version of Raspberry Pi OS,
+I wanted to run it from the CLI, which is possible using [xinit](https://en.wikipedia.org/wiki/Xinit) but
+requires yet more configuration.
+
+So I decided to put this script together to automate the process in hope that that it helps others to enjoy this
+amazing game on their Pis.
+
+## Limitations ##
+
+* Currently this script supports only 32-bit versions of Raspberry Pi OS.
+* It has only been tested against the 'Bullseye' release.
+* It requires the use of the KMS GL driver to get playable performance.
+
+## DISCLAIMER - **USE AT YOUR OWN RISK** ##
+
+This script necessarily makes use of [`sudo`](https://en.wikipedia.org/wiki/Sudo)
+for some operations.
+Some of these will make changes to your Pi that you may not want and/or may break other software.
+e.g. it will increase the size of the
+[swap file](https://www.linux.com/news/all-about-linux-swap-space/) if it's less than 1GB.
+
+I do not claim to be an expert in Linux/Raspberry Pi OS. This works for me, but I am unable to test this script with every combination of hardware & software environment.
+It's possible that I have made an error that will cause unrecoverable damage to your Pi system image and leave it 'bricked'.
+I will not be held responsible for any loss resulting in the use of this script, as per the conditions
+contained in the [LICENSE](LICENSE). I encourage you to read and understand the script contents prior to running it.
+
+I **do not** recommend you use this script if you have valuable data on your Pi and/or the Pi is already in use for
+something important to you.
+
+I **do** recommend you backup your data and use a fresh install of Raspberry Pi OS 'Bullseye' on a spare SD card
+to run this script.
+
+## Installation ##
+
+### Prerequisites ###
+
+The following steps should be done **BEFORE** using the script to prepare your Pi.
+These assume a fresh install of Raspberry Pi OS 'Bullseye' Lite edition.
+Your Pi must also have internet access for this whole process.
+
+1. If you have 1GB or less of RAM (all Pis pre v4), reduce the GPU share to 64MB:
+
+     `sudo raspi-config` -> Performance Options -> GPU Memory
+
+    This is done to give as much RAM as possible to the CPU when building box86
+
+2. Expand the file system to use all of the SD card if you didn't already:
+
+     `sudo raspi-config` -> Advanced Options -> Expand Filesystem
+
+3. Enable the KMS GL driver:
+
+     `sudo raspi-config` -> Advanced Options -> GL Driver -> (Full KMS)
+
+     (You probably want 'Fake KMS' if running on 'Buster' but this hasn't been thoroughly tested)
+
+4. (Optional) Enable SSH if you want to and know how to use it!
+
+     `sudo raspi-config` -> Interface Options -> SSH
+
+5. Update your system:
+
+    `sudo apt update && sudo apt -y upgrade`
+
+6. Reboot!
+
+### Getting the Script ###
+
+You may copy the [install_mgpr_pi.sh](install_mgpr_pi.sh) script from this repo to your Pi anyway you wish
+(e.g. using `scp` if you enabled SSH). This is the only file from this repo required to install `mgpr`.
+Alternatively, clone direct from GitHub on your Pi:
+
+```shell
+sudo apt install git
+git clone https://github.com/neildavis/mgpr-pi.git
+cp mgpr-pi/*.sh ~/
+cd
+```
+
+### Configuring the Script ###
+
+The script makes certain assumptions about the display and orientation of the game.
+These can be overridden by changing the variables at the top of the script to match
+your setup. In particular you may want to change some/all of the variables prefixed
+`mgpr_cfg_` and `mgpr_display_` to suit your needs. See the comments in the script
+for more details.
+
+You may also want to change `debian_package_mirror` to a
+[server nearer to you](https://www.debian.org/mirror/list).
+
+### Running the Script ###
+
+```shell
+./install_mgpr_pi
+```
+
+Wait for the script to finish. It will take some time as box86 takes a while to build.
+
+### Post-install ###
+
+After installation, you'll probably want to reset your GPU memory to 128MB or 256MB using `raspi-config` if you reduced it earlier.
+
+Another reboot is also advised.
+
+## Running MGPR ##
+
+### Running under the X11 Desktop ###
+
+If you're using the full Desktop version of Raspberry Pi OS, you should just be able to launch the `mgpr` executable
+from the `mgpr_v1_4_6_linux` directory directly. e.g. from a new Terminal window:
+
+```shell
+cd mgpr_v1_4_6_linux
+./mgpr
+```
+
+### Running from CLI mode ###
+
+If you're using the 'Lite' version of Raspberry Pi OS and/or booting directly into the CLI console
+the game needs to run under `xinit`. The install script generated two files to enable this:
+
+1. `mgpr_v1_4_6_linux/.xinitrc`
+
+    A `xinit` config file for the X11 server used to run the game.
+    You may need to edit the this file to match your particular display if you didn't configure the script
+    variables beforehand (See [Configuring the script](#configuring-the-script)).
+
+2. `~/bin/mgpr.sh`
+
+     A convenience script to launch the game.
+
+```shell
+~/bin/mgpr.sh
+```
+
+## Known Issues ##
+
+1. Some characters may be missing from the text in the intro screen and also in the
+banner text for 'time', 'score' etc in the game.
+I'm not sure what causes this but it's something to do with the KMS rendering pipeline
+since it doesn't happen with the 'Legacy' GL driver, but the game is unbearably slow
+under that driver without full GL acceleration support.
+
+## Troubleshooting ##
+
+If the installation succeeds but the game doesn't run, try running it directly but prefixing
+the command with `BOX86_LOG=1`. This will cause box86 to spit put lots of debug info that
+may be helpful in resolving the issue. e.g.
+
+```shell
+cd mgpr_v1_4_6_linux
+BOX86_LOG=1 ./mgpr
+```
+
+## TODO: ##
+
+1. Add support for box86 under 64-bit Raspberry Pi OS.
+2. Try using [box64](https://github.com/ptitSeb/box64) on 64-bit OS using the x64 Linux `mgpr` binary.
+3. Make configuration more friendly. Perhaps interactive?
+
+## Contributing ##
+
+Please feel free to fork and send pull requests.
---
+++ install_mgpr_pi.sh
@@ -0,0 +1,327 @@
+#!/bin/env bash
+
+swap_mb_required=1024 # Min 1GB swap is recommended to compile box86
+
+box86_repo="https://github.com/ptitSeb/box86.git"
+box86_ver="v0.2.8"
+box86_build_dir="${HOME}/code/box86"
+
+badown_repo="https://github.com/stck-lzm/badown.git"  # to help download from mediafire
+badown_ver="master"
+badown_dir="${HOME}/badown"
+
+# Change this to your local Debian mirror for faster downloads
+# https://www.debian.org/mirror/list
+debian_package_mirror="ftp.us.debian.org/debian/"
+
+mgpr_linux_tar_gz_url="https://www.mediafire.com/file/0c68v3eb4m4wgbd/mgpr_v1_4_6_linux_04_04_2016.tar.gz"
+mgpr_dest_dir="${HOME}/mgpr_v1_4_6_linux" # last path component MUST match TLD of mgpr tarball
+
+# These lines affect the mgpr config file generated in mgpr's 'cfg' dir
+mgpr_cfg_name="nn"            # The name of the configuration file.
+mgpr_cfg_display_width=800    # if TATE mode is applied this will become height
+mgpr_cfg_display_height=480   # if TATE mode is applied this will become width
+mgpr_cfg_fullscreen=yes       # Change to 'no' to disable fullscreen use (e.g. when running in a desktop window)
+mgpr_cfg_tate_mode=yes        # TATE mode rotates the game to play in portrait making better use of screen space.
+mgpr_cfg_playfield_width=640  # if TATE mode is applied this will become height
+mgpr_cfg_playfield_height=480 # if TATE mode is applied this will become width
+mgpr_cfg_playfield_pos_x=60   # if TATE mode is applied this will become y pos
+mgpr_cfg_playfield_pos_y=0    # if TATE mode is applied this will become x pos
+# These lines affect the .xinitrc file generated to run mgpr under X11 from the CLI console. 
+mgpr_display_output_id="HDMI-1" # Change to 'DSI-1' if using DSI display (e.g. official 7" touch screen)
+mgpr_display_rotate="normal"    # Change to 'inverted' to rotate the screen 180 degrees in TATE mode.
+
+# Install the build environment
+echo "Installing build tools and dependenices ..."
+sudo apt -y install git build-essential cmake xinit x11-xserver-utils libpulse0 gawk \
+  libegl1-mesa-dev  libgles2-mesa-dev libgl1-mesa-dev libgbm-dev libdrm-dev
+
+# Sort out swap file
+source /etc/dphys-swapfile
+if [ $CONF_SWAPSIZE -lt $swap_mb_required ]; then 
+  echo "Swap size ${CONF_SWAPSIZE} MB is insufficient. Raising to 1024 MB"
+  sudo dphys-swapfile swapoff
+  sudo sed -i 's/CONF_SWAPSIZE='"${CONF_SWAPSIZE}"'/CONF_SWAPSIZE='"${swap_mb_required}"'/g' /etc/dphys-swapfile   
+  sudo dphys-swapfile setup
+  sudo dphys-swapfile swapon
+else 
+  echo "Swap size ${CONF_SWAPSIZE} MB is sufficient"
+fi
+
+# Let's build box86
+if [[ ! -d "${box86_build_dir}" ]]; then
+  echo "Fetching box86 version ${box86_ver} from ${box86_repo}"
+  mkdir -p "${box86_build_dir}"
+  git -c advice.detachedHead=false clone --depth=1 -b "${box86_ver}" "${box86_repo}" "${box86_build_dir}"
+fi
+if [[ ! -f "${box86_build_dir}/box86" ]]; then
+  cd "${box86_build_dir}"
+  box86_rpi_build_flag="-DRPI$(grep -Po 'Raspberry Pi \K[[:digit:]+](?= Model)' /proc/cpuinfo)=1"
+  echo "Building box86 with ${box86_rpi_build_flag} ..."
+  cmake . "${box86_rpi_build_flag}" -DCMAKE_BUILD_TYPE=RelWithDebInfo
+  make -j$(nproc)
+  sudo make install
+  sudo systemctl restart systemd-binfmt
+  cd "${HOME}"
+else
+  echo "Skipping box86 build. Already built"
+fi
+
+# Let's grab badown to help us download MGPR from mediafire
+if [[ ! -d "${badown_dir}" ]]; then
+  echo "Fetching badwown version ${badown_ver} from ${badown_repo}"
+  mkdir -p "${badown_dir}"
+  git -c advice.detachedHead=false clone --depth=1 -b "${badown_ver}" "${badown_repo}" "${badown_dir}"
+else
+  echo "badown has laready been downloaded"
+fi
+
+# Let's grab Monaco GP Remake
+mgpr_tar_gz_dest="mgpr_v1_4_6_linux_04_04_2016.tar.gz"
+if [[ ! -f "${mgpr_tar_gz_dest}" ]]; then
+  echo "Downloading mgpr from ${mgpr_linux_tar_gz_url} ..."
+  "${badown_dir}"/badown "${mgpr_linux_tar_gz_url}"
+else
+  echo "MGPR Linux has already been downloaded"
+fi
+if [[ ! -d "${mgpr_dest_dir}" ]]; then
+  echo "Extracting ${mgpr_tar_gz_dest} ..."
+  tar -xf "${mgpr_tar_gz_dest}"
+  #rm "${mgpr_tar_gz_dest}"
+fi
+if [[ ! -f "${mgpr_dest_dir}/mgpr" ]]; then
+  cp "${mgpr_dest_dir}/linux_builds/static_32bit/mgpr" "${mgpr_dest_dir}/"
+fi
+if [[ $(ls -l "${mgpr_dest_dir}"/*.so* | wc -l) -eq 0 ]]; then
+  echo "Fetching i386 pkg dependencies for mgpr"
+  mkdir -p "${mgpr_dest_dir}"/debs
+  declare -a pkgs_i386=(\
+    "http://${debian_package_mirror}pool/main/libd/libdumb/libdumb1_0.9.3-6+b3_i386.deb"\
+    "http://${debian_package_mirror}pool/main/f/flac/libflac8_1.3.3-2+deb11u1_i386.deb"\
+    "http://${debian_package_mirror}pool/main/libv/libvorbis/libvorbisfile3_1.3.7-1_i386.deb"\
+    "http://${debian_package_mirror}pool/main/o/openal-soft/libopenal1_1.19.1-2_i386.deb"\
+    "http://${debian_package_mirror}pool/main/s/sndio/libsndio7.0_1.5.0-3_i386.deb"\
+    "http://${debian_package_mirror}pool/main/libb/libbsd/libbsd0_0.11.3-1_i386.deb"\
+    "http://${debian_package_mirror}pool/main/libm/libmd/libmd0_1.0.3-3_i386.deb"
+   )
+  for pkg in "${pkgs_i386[@]}"; do
+    wget -P "${mgpr_dest_dir}"/debs "${pkg}"
+  done
+  for deb in "${mgpr_dest_dir}"/debs/*; do
+    dpkg -x "${deb}" "${mgpr_dest_dir}"/debs
+  done
+  find "${mgpr_dest_dir}" -name *.so* -print0 | xargs -0 mv -t "${mgpr_dest_dir}"
+  rm -rf "${mgpr_dest_dir}"/debs
+else 
+  echo "i386 pkg dependencies for mgpr have already been fetched"
+fi
+
+# Create .xinitrc for launching mgpr from CLI
+mgpr_xinit_rc="${mgpr_dest_dir}/.xinitrc"
+if [[ ! -f "${mgpr_xinit_rc}" ]]; then
+echo "Creating MGPR .xinitrc script"
+cat <<EOF > "${mgpr_xinit_rc}"
+cd "${mgpr_dest_dir}"
+DISPLAY=:0 xrandr --output ${mgpr_display_output_id} --rotate ${mgpr_display_rotate}
+./mgpr -cfg $mgpr_cfg_name
+EOF
+else
+  echo "MGPR .xinitrc ${mgpr_dest_dir}/.xinitrc already exists"
+fi
+
+# Create user scripts to launch
+mkdir -p "${HOME}"/bin
+mgpr_launch_sh="${HOME}/bin/mgpr.sh"
+if [[ ! -f "${mgpr_launch_sh}" ]]; then
+echo "Creating MGPR launch script"
+cat <<EOF > "${mgpr_launch_sh}"
+#!/bin/env bash
+XINITRC=${mgpr_dest_dir}/.xinitrc xinit -- :0 -quiet vt\$XDG_VTNR 
+EOF
+  chmod a+x "${mgpr_launch_sh}"
+else
+  echo "MGPR launch script ${HOME}/bin/mgpr.sh already exists"
+fi
+
+# Create MGPR config
+echo "Creating MGPR cfg (${mgpr_dest_dir}/cfg/$mgpr_cfg_name)"
+mkdir -p "${mgpr_dest_dir}"/cfg
+cat << EOF > "${mgpr_dest_dir}"/cfg/"$mgpr_cfg_name"
+DISPLAY resolution "${mgpr_cfg_display_width}x${mgpr_cfg_display_height}"
+DISPLAY fullscreen ${mgpr_cfg_fullscreen}
+DISPLAY window_frame no
+DISPLAY set_window_pos no
+DISPLAY window_pos_x 0
+DISPLAY window_pos_y 0
+DISPLAY scoreboard_mode "private"
+DISPLAY playfield_w ${mgpr_cfg_playfield_width}
+DISPLAY playfield_h ${mgpr_cfg_playfield_height}
+DISPLAY playfield_x ${mgpr_cfg_playfield_pos_x}
+DISPLAY playfield_y ${mgpr_cfg_playfield_pos_y}
+DISPLAY rotate ${mgpr_cfg_tate_mode}
+DISPLAY flip no
+DISPLAY mirror no
+DISPLAY filter yes
+DISPLAY open_gl no
+DISPLAY menu_contrast 20
+DISPLAY playfield_stage yes
+DISPLAY playfield_time yes
+DISPLAY playfield_score yes
+DISPLAY playfield_bonus yes
+DISPLAY playfield_gear yes
+DISPLAY playfield_infinite_lives no
+DISPLAY playfield_get_ready yes
+SOUND volume_master 100
+SOUND radio_chat_enable yes
+SOUND radio_chat_vol 100
+SOUND attract_mode_sounds yes
+SOUND loop_goal yes
+SOUND loop_1st yes
+SOUND loop_2nd no
+SOUND loop_3rd no
+SOUND loop_4th no
+SOUND loop_5th no
+SOUND volume_goal 100
+SOUND volume_1st 80
+SOUND volume_2nd 80
+SOUND volume_3rd 80
+SOUND volume_4th 80
+SOUND volume_5th 80
+SOUND cheers_goal 100
+SOUND cheers_1st 50
+SOUND cheers_2nd 40
+SOUND cheers_3rd 30
+SOUND cheers_4th 20
+SOUND cheers_5th 10
+SOUND volume_welcome 80
+SOUND low_gear_base 0.8
+SOUND low_gear_multiplier 20
+SOUND hi_gear_base 0.4
+SOUND hi_gear_multiplier 100
+SOUND filename_welcome "trumpet.wav"
+SOUND filename_goal "music.wav"
+SOUND filename_1st "music.wav"
+SOUND filename_2nd "trumpet.wav"
+SOUND filename_3rd "trumpet.wav"
+SOUND filename_4th "trumpet.wav"
+SOUND filename_5th "trumpet.wav"
+SOUND wav_bonus_overtake "beep_clean.wav"
+SOUND vol_bonus_overtake 100
+SOUND wav_bonus_award "beep_distorted.wav"
+SOUND vol_bonus_award 100
+SOUND wav_bonus_bridge "beep_distorted.wav"
+SOUND vol_bonus_bridge 100
+ARTWORK enable_bezel_artwork no
+ARTWORK bezel_filename "scoreboard_w1024.bmp"
+ARTWORK bezel_w 0
+ARTWORK bezel_h 0
+ARTWORK bezel_x 0
+ARTWORK bezel_y 0
+ARTWORK enable_led_artwork no
+ARTWORK led_filename "speccy_multi3.bmp"
+ARTWORK time_w 20
+ARTWORK time_h 20
+ARTWORK time_x 0
+ARTWORK time_y 0
+ARTWORK time_id 0
+ARTWORK todays_best_w 20
+ARTWORK todays_best_h 20
+ARTWORK todays_best_1_x 0
+ARTWORK todays_best_1_y 20
+ARTWORK todays_best_1_id 1
+ARTWORK todays_best_2_x 0
+ARTWORK todays_best_2_y 40
+ARTWORK todays_best_2_id 2
+ARTWORK todays_best_3_x 0
+ARTWORK todays_best_3_y 60
+ARTWORK todays_best_3_id 3
+ARTWORK todays_best_4_x 0
+ARTWORK todays_best_4_y 80
+ARTWORK todays_best_4_id 4
+ARTWORK todays_best_5_x 0
+ARTWORK todays_best_5_y 100
+ARTWORK todays_best_5_id 5
+ARTWORK your_score_w 20
+ARTWORK your_score_h 20
+ARTWORK your_score_x 0
+ARTWORK your_score_y 120
+ARTWORK your_score_id 6
+ARTWORK players_to_date_w 20
+ARTWORK players_to_date_h 20
+ARTWORK players_to_date_x 0
+ARTWORK players_to_date_y 140
+ARTWORK players_to_date_id 7
+ARTWORK ranking_w 20
+ARTWORK ranking_h 20
+ARTWORK ranking_x 0
+ARTWORK ranking_y 160
+ARTWORK ranking_id 8
+ARTWORK super_racer_w 20
+ARTWORK super_racer_h 20
+ARTWORK super_racer_x 0
+ARTWORK super_racer_y 180
+ARTWORK overtake_filename "overtake_leds.bmp"
+ARTWORK overtake_row_upper_w 20
+ARTWORK overtake_row_upper_h 20
+ARTWORK overtake_row_upper_x 0
+ARTWORK overtake_row_upper_y 200
+ARTWORK overtake_row_lower_w 20
+ARTWORK overtake_row_lower_h 20
+ARTWORK overtake_row_lower_x 0
+ARTWORK overtake_row_lower_y 220
+CONTROL steering_device "keys"
+CONTROL steering_invert no
+CONTROL steering_sensitivity 50
+CONTROL steering_weight 50
+CONTROL accelerator_device "keys"
+CONTROL accelerator_invert no
+CONTROL accelerator_range "0 to +1"
+CONTROL gear_change_device "keys"
+CONTROL gear_method "toggle"
+CONTROL coin_a_device "keys"
+CONTROL coin_b_device "keys"
+CONTROL start_device "keys"
+CONTROL key_accel "n"
+CONTROL key_gear "m"
+CONTROL key_coin_a "5"
+CONTROL key_coin_b "6"
+CONTROL key_start "1"
+CONTROL key_left "z"
+CONTROL key_right "x"
+CONTROL key_exit "escape"
+GAMEPLAY game_track "game_arcade.trk"
+GAMEPLAY attract_track "attract_arcade.trk"
+GAMEPLAY time_bridge_grace 5
+GAMEPLAY time_puddle 350
+GAMEPLAY puddle_steering 90
+GAMEPLAY verge_trigger 40
+GAMEPLAY verge_slow_down 70
+GAMEPLAY marsh_trigger 50
+GAMEPLAY marsh_slow_down 60
+GAMEPLAY marsh_steering 90
+GAMEPLAY surface_wobble_rate 5
+GAMEPLAY time_grace_scenery 7
+GAMEPLAY time_grace_verge 7
+GAMEPLAY brake_low_gear 2
+GAMEPLAY brake_high_gear 2
+GAMEPLAY brake_wrong_gear 1.7
+GAMEPLAY arcade_acceleration yes
+GAMEPLAY brake_immediate 50
+GAMEPLAY vertical_climb 50
+GAMEPLAY vertical_fall 50
+GAMEPLAY blink_frames_on 6
+GAMEPLAY blink_frames_off 6
+GAMEPLAY pro_monaco_gp_mode no
+GAMEPLAY debug_track no
+GAMEPLAY tarmac_normal_play_r 62
+GAMEPLAY tarmac_normal_play_g 62
+GAMEPLAY tarmac_normal_play_b 62
+GAMEPLAY tarmac_extended_play_r 0
+GAMEPLAY tarmac_extended_play_g 0
+GAMEPLAY tarmac_extended_play_b 72
+GAMEPLAY night_lights_fix yes
+GAMEPLAY attract_mode_hiscore yes
+GAMEPLAY end_game_you_placed no
+GAMEPLAY end_game_stats no
+EOF
+cd "${HOME}"
