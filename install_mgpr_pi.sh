214d664e8c00379207959d59788ae99e75ec7e9c
Neil Davis <neildavis@users.noreply.github.com>
2023-03-16 17:12:33 +0000

    Update box64 version

--- install_mgpr_pi.sh
+++ install_mgpr_pi.sh
@@ -29,7 +29,7 @@ box86_ver="v0.2.8"
 box86_build_dir="${HOME}/code/box86"
 box86_make_jobs_concurrent=1  # make -j param. Will be set to $(nproc) if you have 4GB+ RAM
 box64_repo="https://github.com/ptitSeb/box64.git"
-box64_ver="main" # Until a new release tag is made
+box64_ver="v0.2.2" # Until a new release tag is made
 box64_build_dir="${HOME}/code/box64"
 box64_make_jobs_concurrent=1  # make -j param. Will be set to $(nproc) if you have 4GB+ RAM
 
@@ -56,8 +56,8 @@ mgpr_cfg_playfield_height=480 # if TATE mode is applied this will become width
 mgpr_cfg_playfield_pos_x=80   # if TATE mode is applied this will become y pos
 mgpr_cfg_playfield_pos_y=0    # if TATE mode is applied this will become x pos
 # These lines affect the .xinitrc file generated to run mgpr under X11 from the CLI console. 
-mgpr_display_output_id="HDMI-1" # Change to 'DSI-1' if using DSI display (e.g. official 7" touch screen)
-mgpr_display_rotate="normal"    # Change to 'inverted' to rotate the screen 180 degrees in TATE mode.
+mgpr_display_output_id="DSI-1" # Change to 'DSI-1' if using DSI display (e.g. official 7" touch screen)
+mgpr_display_rotate="inverted"    # Change to 'inverted' to rotate the screen 180 degrees in TATE mode.
 
 
 # Helper func to determine if we're running on a 64-bit system
